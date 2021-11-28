516640a5a610e39239daada479cbf7c8f1f8753a
sayra17 <agussayra03@gmail.com>
2021-11-28 07:17:10 +0700

    Initial commit

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number= 17;
+var string= "NIOMIC";
+var bolean= true;
+var kosong;
+var array= [17,'niomic'];
+
+ console.log(number);
+ console.log(string);                                                                                                                   
+ console.log(bolean);
+ console.log(kosong);
+ console.log(array);
